201dd4a631957d96279383821f6c8f572fd94508
XJIE6 <YuryKravchenko@Urijs-MacBook-Air.local>
2016-07-26 16:28:51 +0300

    first try with xcode

---
+++ prufercoder/prufercoder/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  prufercoder
+//
+//  Created by Юрий Кравченко on 26/07/16.
+//  Copyright © 2016 Юрий Кравченко. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
